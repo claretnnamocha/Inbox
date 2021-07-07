a548bf089ac88e2d8efc73fea1e932218a47c410
Claret Nnamocha <devclareo@gmail.com>
2021-07-07 18:11:11 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 
 const jobs = require("./jobs");
 
-jobs.deploy("My First Contract").then((contract) => {
-  console.log(`Deployed to ${contract.options.address}`);
-});
+// jobs.deploy("My First Contract").then((contract) => {
+//   console.log(`Deployed to ${contract.options.address}`);
+// });
